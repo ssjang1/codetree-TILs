327c5968e96fc8d9d930ce837828e8d3df91a794
Jang SeongSik <137245532+ssjang1@users.noreply.github.com>
2024-04-05 22:34:28 +0900

    Title: 알파벳에 따른 평가, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240405/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,16 @@
+word = input().super()
+
+if word == 'S':
+    result = 'Superior'
+elif word == 'A':
+    result = 'Excellent'
+elif word =='B':
+    result = 'Good'
+elif word =='C':
+    result = 'Usually'
+elif word =='D':
+    result = 'Effort'
+else:
+    result = 'Failure'
+
+print(result)
