ef884be516c4f8c215920402d117cb6ae128474e
Jang SeongSik <137245532+ssjang1@users.noreply.github.com>
2024-04-10 22:27:55 +0900

    Title: 장학금, Time: 103ms, Memory: 29MB, Status: Wrong Answer - CodeTree

---
+++ 240410/장학금/scholarship.py
@@ -0,0 +1,11 @@
+middle, final = map(int, input().split(' '))
+
+if middle >=90:
+    if final>=95:
+        print(10)
+    elif final>=90:
+        print(5)
+    else:
+        print(0)
+else:
+    print(0)
