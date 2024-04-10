eac9c710da8bd12ba5a19fd7126832676e8d1133
Jang SeongSik <137245532+ssjang1@users.noreply.github.com>
2024-04-10 23:00:02 +0900

    Title: 최댓값 구하기, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240410/최댓값 구하기/maximum-value.py
+++ 240410/최댓값 구하기/maximum-value.py
@@ -1,3 +1,11 @@
 a,b,c = map(int, input().split(' '))
 
-print(max([a,b,c]))
+maximum = 0
+if a> maximum:
+    maximum = a
+if b> maximum:
+    maximum = b
+if c >maximum:
+    maximum = c
+
+print(maximum)
