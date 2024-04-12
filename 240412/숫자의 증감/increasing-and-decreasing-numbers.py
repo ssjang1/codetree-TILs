94536b89d11b83a88e294ecc58c8e0334efc70bf
Jang SeongSik <137245532+ssjang1@users.noreply.github.com>
2024-04-12 21:32:07 +0900

    Title: 숫자의 증감, Time: 129ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240412/숫자의 증감/increasing-and-decreasing-numbers.py
@@ -0,0 +1,9 @@
+c, n = input().split(' ')
+
+if c == 'A':
+    for i in range(1, int(n)+1):
+        print(i, end=' ')
+
+if c == 'D':
+    for i in range(int(n), 0, -1):
+        print(i, end =' ')
