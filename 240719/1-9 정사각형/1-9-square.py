8e31a334d2df39b1998866b7dbd54ea5c48ff69e
Jang SeongSik <137245532+ssjang1@users.noreply.github.com>
2024-07-19 01:03:08 +0900

    Title: 1-9 정사각형, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

--- 240719/1-9 정사각형/1-9-square.py
+++ 240719/1-9 정사각형/1-9-square.py
@@ -4,7 +4,7 @@ cnt=1
 for i in range(n):
     for j in range(n):
         print(cnt, end='')
-        cnt+=1
-        if cnt ==9:
+        cnt +=1
+        if cnt ==10:
             cnt=1
     print()
