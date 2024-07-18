f7bb4655824819d8a2f631e200636cf919290fc0
Jang SeongSik <137245532+ssjang1@users.noreply.github.com>
2024-07-18 23:47:00 +0900

    Title: 별 그리기, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240718/별 그리기/star-drawing.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+for i in range(1,n+1):
+    print(' '*(n-i),end='')
+    print('*'*(2*i-1),end='')
+    print()
+
+for j in range(n-1,0,-1):
+    print(' '*(n-j),end='')
+    print('*'*(2*j-1),end='')
+    print()
