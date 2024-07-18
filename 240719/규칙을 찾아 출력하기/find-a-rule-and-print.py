abbc87d1437a51c7257aa374f307d456118c1a2e
Jang SeongSik <137245532+ssjang1@users.noreply.github.com>
2024-07-19 00:34:50 +0900

    Title: 규칙을 찾아 출력하기, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240719/규칙을 찾아 출력하기/find-a-rule-and-print.py
+++ 240719/규칙을 찾아 출력하기/find-a-rule-and-print.py
@@ -4,5 +4,6 @@ for i in range(1,n+1):
     if i == 1:
         print(' '.join('*'*n))
     else:
-        print(' '.join('*'*(i-1)),end=' '*(2*n-1-i))
+        print(' '.join('*'*(i-1)),end='')
+        print(' '*(2*n-1-i),end='')
         print('*')
