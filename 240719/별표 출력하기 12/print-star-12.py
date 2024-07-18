43beea3e24d5b44b01fcc9e071ab30e7419cf957
Jang SeongSik <137245532+ssjang1@users.noreply.github.com>
2024-07-19 00:26:49 +0900

    Title: 별표 출력하기 12, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240719/별표 출력하기 12/print-star-12.py
@@ -0,0 +1,19 @@
+n= int(input())
+
+if n%2 == 1:
+    m=n-1
+else:
+    m=n
+
+now = int(n/2)
+for i in range(1,n+1):
+    if i ==1:
+        print(' '.join('*'*n))
+    elif i==2:
+        print(' '*2,end='')
+        print(' '.join('*'*now))
+        now -= 1
+    else:
+        print(' '*(2*m-4*now+2),end='')
+        print(' '.join('*'*now))
+        now -= 1
