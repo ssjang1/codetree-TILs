cc1c86d833d26e1c1d9e4db8627964156ddc3c71
Jang SeongSik <137245532+ssjang1@users.noreply.github.com>
2024-07-20 00:03:14 +0900

    Title: 알파벳 출력하기, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240720/알파벳 출력하기/print-alphabet.py
+++ 240720/알파벳 출력하기/print-alphabet.py
@@ -7,6 +7,6 @@ for i in range(n):
     for j in range(i+1):
         print(chr(start),end='')
         start +=1
-        if start == finish+1:
+        if start == finish:
             start = ord('A')
     print()
