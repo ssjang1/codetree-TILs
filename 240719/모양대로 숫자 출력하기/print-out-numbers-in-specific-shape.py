9f10628146f4083608e1346a5ea9cd7ee91e2573
Jang SeongSik <137245532+ssjang1@users.noreply.github.com>
2024-07-19 23:36:59 +0900

    Title: 모양대로 숫자 출력하기, Time: 103ms, Memory: 24MB, Status: Passed - CodeTree

--- 240719/모양대로 숫자 출력하기/print-out-numbers-in-specific-shape.py
+++ 240719/모양대로 숫자 출력하기/print-out-numbers-in-specific-shape.py
@@ -3,8 +3,8 @@ n = int(input())
 for i in range(n):
     string =''
     # string.append(' '*i)
-    string += ' '*i
+    string += (' ' + ' ')*i
     for j in range(n-i):
         # string.append(n-j)
-        string += str(n-j -i)
-    print(' '.join(string))
+        string += str(n-j -i) + ' '
+    print(string)
