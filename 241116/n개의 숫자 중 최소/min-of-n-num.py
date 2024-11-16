eabc74bcad3bb617cdab17134b54d3d32908e7f6
Jang SeongSik <137245532+ssjang1@users.noreply.github.com>
2024-11-16 15:02:48 +0900

    Title: n개의 숫자 중 최소, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 241116/n개의 숫자 중 최소/min-of-n-num.py
+++ 241116/n개의 숫자 중 최소/min-of-n-num.py
@@ -1,10 +1,12 @@
 n = int(input())
 a = list(map(int,input().strip().split(' ')))
 
-min_v = a[0]
+# min_v = a[0]
 
-for e in a[1:]:
-    if min_v > e:
-        min_v = e
+# for e in a[1:]:
+#     if min_v > e:
+#         min_v = e
 
-print(min_v, a.count(min_v), end=" ")
+# print(min_v, a.count(min_v), end=" ")
+
+print(min(a),a.count(min(a)),end=" ")
