a9444277301171a9b2feaa1dc708161c5143d8a8
Jang SeongSik <137245532+ssjang1@users.noreply.github.com>
2024-11-16 14:32:13 +0900

    Title: 배열 놀이, Time: 76ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 241116/배열 놀이/play-with-array.py
@@ -0,0 +1,15 @@
+n,q = map(int,input().split(' '))
+wonso = list(map(int,input().split(' ')))
+
+for i in range(q):
+    line = list(map(int,input().split(' ')))
+    if line[0] == 1:
+        print(wonso[line[1]-1])
+    elif line[0] ==2:
+        if line[1] in wonso:
+            print(wonso.index(line[1])+1)
+        else:
+            print(0)
+    elif line[0] ==3:
+        for i in range(line[1]-1,line[2]):
+            print(wonso[i],end=' ')
