0645593ee4f5bdd2d8495e7a4fc08c91926c5a07
Jang SeongSik <137245532+ssjang1@users.noreply.github.com>
2024-11-16 13:52:11 +0900

    Title: 코로나 메뉴얼2, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 241116/코로나 메뉴얼2/covid-manual2.py
+++ 241116/코로나 메뉴얼2/covid-manual2.py
@@ -1,1 +1,22 @@
-.
+a,b,c,d,e = 0,0,0,0,0
+for i in range(3):
+    cold, temperature = input().split(' ')
+    temp = float(temperature)
+    if cold == "Y" and temp >=37:
+        a+=1
+    elif cold == "N" and temp >=37:
+        b+=1
+    elif cold =="Y" and temp <37:
+        c+=1
+    else:
+        d+=1
+    
+if a>=2:
+    e+=1
+
+print(a,end=' ')
+print(b,end=' ')
+print(c,end=' ')
+print(d,end=' ')
+if e:
+    print("E")
