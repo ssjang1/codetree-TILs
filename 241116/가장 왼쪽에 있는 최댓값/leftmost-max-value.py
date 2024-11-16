e399f2f26f7e78f6e9c7cab69e4d4d0b63d7c9a6
Jang SeongSik <137245532+ssjang1@users.noreply.github.com>
2024-11-16 15:25:47 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 58ms, Memory: 16MB, Status: Passed - CodeTree

--- 241116/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
+++ 241116/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
@@ -1,7 +1,7 @@
 n = int(input())
 line = list(map(int,input().strip().split(' ')))
 
-while len(line)>=1:
+while len(line)>0:
     print(line.index(max(line))+1,end=" ")
     line = line[:line.index(max(line))]
     
