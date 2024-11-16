21099b1dd7aa044b069cce40f10291b65055a1e9
Jang SeongSik <137245532+ssjang1@users.noreply.github.com>
2024-11-16 15:24:11 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 48ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241116/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
+++ 241116/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
@@ -4,5 +4,4 @@ line = list(map(int,input().strip().split(' ')))
 while len(line)>=1:
     print(line.index(max(line))+1,end=" ")
     line = line[:line.index(max(line))]
-    if len(line) <=1:
-        break
+    
