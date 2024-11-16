7d64c5ba076fc1cdb4bc551e8bf42405bb1e0ea5
Jang SeongSik <137245532+ssjang1@users.noreply.github.com>
2024-11-16 14:00:33 +0900

    Title: 나눗셈의 나머지, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241116/나눗셈의 나머지/remainder-of-division.py
@@ -0,0 +1,12 @@
+a,b = map(int,input().split(' '))
+check = []
+while a >1:
+    a=a//b
+    remain = a%b
+    check.append(remain)
+
+result = 0
+for i in set(check):
+    result += check.count(i)**2
+
+print(result)
