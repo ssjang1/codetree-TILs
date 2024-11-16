bab341c810b9111d8b204095b66db512b18eddef
Jang SeongSik <137245532+ssjang1@users.noreply.github.com>
2024-11-16 15:39:18 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 80ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 241116/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
+++ 241116/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
@@ -4,7 +4,7 @@ line = list(map(int,input().strip().split(' ')))
 
 maxi =0
 for i in range(n):
-    for j in range(i:):
+    for j in range(i,n):
         if line[j]-line[i]>maxi:
             maxi = line[j]-line[i]
 
