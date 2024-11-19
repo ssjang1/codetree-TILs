471de7a8722b6e511ac7c092bb1289d7bfd98629
Jang SeongSik <137245532+ssjang1@users.noreply.github.com>
2024-11-19 17:43:17 +0900

    Title: 숫자로 이루어진 사각형, Time: 65ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 241119/숫자로 이루어진 사각형/rectangle-with-a-number.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+def number_suare(n):
+    start = 1
+    for i in range(n):
+        for j in range(n):
+            print(start,end=' ')
+            if start ==9:
+                start==0
+            start +=1
+        print()
+number_suare(n)
