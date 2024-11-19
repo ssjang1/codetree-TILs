68da1f9969251a71c80b20b8e7016488838a3d96
Jang SeongSik <137245532+ssjang1@users.noreply.github.com>
2024-11-19 16:22:53 +0900

    Title: 정수만 추출하기, Time: 76ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 241119/정수만 추출하기/extract-only-integers.py
@@ -0,0 +1,18 @@
+a,b = input().split(' ')
+
+A = ''
+B = ''
+
+for i in a:
+    if a.isdigit():
+        A +=a
+    else:
+        break
+
+for i in b:
+    if b.isdigit():
+        B +=b
+    else:
+        break
+
+print(int(A)+int(B))
