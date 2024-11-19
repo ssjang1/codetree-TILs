19a8da14b41f94da74400e46bd964c04480d30c2
Jang SeongSik <137245532+ssjang1@users.noreply.github.com>
2024-11-19 15:18:52 +0900

    Title: 문자열 놀이, Time: 51ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241119/문자열 놀이/play-with-string.py
@@ -0,0 +1,15 @@
+s,q = input().split(' ')
+q = int(q)
+
+for i in range(q):
+    a,b,c = input().split(' ')
+    if a =='1':
+        b = int(b)
+        c = int(c)
+        check = list(s)
+        check[b-1], check[c-1] = check[c-1], check[b-1]
+        result = ''.join(check)
+        print(result)
+    elif a=='2':
+        result = s.replace(b,c)
+        print(result)
