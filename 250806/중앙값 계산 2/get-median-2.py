2483888bff88747e11cb11caf2ca5f2bf8d8a6f1
Jang SeongSik <137245532+ssjang1@users.noreply.github.com>
2025-08-06 16:52:07 +0900

    Title: 중앙값 계산 2, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250806/중앙값 계산 2/get-median-2.py
+++ 250806/중앙값 계산 2/get-median-2.py
@@ -3,9 +3,15 @@ arr = list(map(int, input().split()))
 
 # Please write your code here.
 
+# for i in range(len(arr)):
+#     if i%2==0:
+#         if i!=n-1 or i !=n-2:
+#             print(arr[i//2], end=' ')
+#         else:
+#             print(arr[i//2])  중앙값이 값의 크기의 중앙값이었음
+
 for i in range(len(arr)):
     if i%2==0:
-        if i!=n-1 or i !=n-2:
-            print(arr[i//2], end=' ')
-        else:
-            print(arr[i//2])
+        check = arr[:i+1]
+        check.sort()
+        print(check[i//2],end=' ')
