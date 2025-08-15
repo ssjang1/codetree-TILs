b44080f1ec7e925501e5fb7c117594ae5daf79f7
Jang SeongSik <137245532+ssjang1@users.noreply.github.com>
2025-08-15 12:14:34 +0900

    Title: 원점으로부터의 거리, Time: 68ms, Memory: 17MB, Status: Passed - Codetree

--- 250815/원점으로부터의 거리/distance-from-origin.py
+++ 250815/원점으로부터의 거리/distance-from-origin.py
@@ -1,5 +1,5 @@
 n = int(input())
-points = [(int(i), map(int, input().split())) for i in range(1,n+1)]
+points = [(int(i), tuple(map(int, input().split()))) for i in range(1,n+1)]
 
 # Please write your code here.
 
@@ -10,4 +10,10 @@ class Point:
         self.y = y
 
 dots = []
-for num,x,y in points:
+for num, (x,y) in points:
+    dots.append(Point(num,x,y))
+
+dots.sort(key = lambda x: (abs(x.x)+abs(x.y),x.num))
+
+for d in dots:
+    print(d.num)
