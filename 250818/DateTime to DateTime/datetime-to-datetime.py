c17a3c2cca1fa033b403f6cdcae2c986c9fb1e10
Jang SeongSik <137245532+ssjang1@users.noreply.github.com>
2025-08-18 14:18:10 +0900

    Title: DateTime to DateTime, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250818/DateTime to DateTime/datetime-to-datetime.py
+++ 250818/DateTime to DateTime/datetime-to-datetime.py
@@ -5,7 +5,7 @@ a, b, c = map(int, input().split())
 m_sum = a * 24 * 60 + b *60 + c
 m_standard = 11 * 24 * 60 + 11 * 60 + 11
 
-if m_sum > m_standard:
+if m_sum >= m_standard:
     print(m_sum-m_standard)
 else:
     print(-1)
